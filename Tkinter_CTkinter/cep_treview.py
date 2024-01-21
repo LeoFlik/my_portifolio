1193f045f187d63252788b51337a768fc1281dad
LeoFlik <leo2071@gmail.com>
2024-01-21 07:20:37 -0300

    Files Updated

--- Tkinter_CTkinter/cep_treview.py
+++ Tkinter_CTkinter/cep_treview.py
@@ -1,4 +1,5 @@
 from customtkinter import *
+from customtkinter import filedialog
 from CTkMessagebox import CTkMessagebox
 import tkinter
 from tkinter import ttk
@@ -20,59 +21,92 @@ root.title("Cep treview")
 set_appearance_mode("dark")
 set_default_color_theme("dark-blue")
 
-def search():
+
+
+def automatic_search():
+        caminho_arquivo = filedialog.askopenfilename(
+        title="Selecione um arquivo",
+        filetypes=[("Texto", "*.txt")]
+    )
+        if caminho_arquivo:
+            with open(caminho_arquivo,'+r') as file:
+              input = file.readlines()
+              lista_ceps =[cep.strip() for cep in input]
+              print(lista_ceps)
+    
+        search(lista_ceps)
+    
+
+def search(dados):
     # Desabilitar o botão enquanto a busca está em andamento
     
-
-    url = "https://buscacepinter.correios.com.br/app/endereco/index.php"
-
-    drive.get(url)
-    pyautogui.sleep(2)
-    data = cep_input.get()
-    drive.find_element(By.NAME, "endereco").send_keys(data)
-    pyautogui.sleep(seconds=0.5)
-    data = cep_input.get()
-    pyautogui.sleep(1)
-    drive.find_element(By.NAME, "btn_pesquisar").click()
-    pyautogui.sleep(1)
+    for i in dados:
+            if i != 'CEP':
+                print(i)
+                data = i
 
 
-    logradouro = drive.find_element(
-        By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[1]'
-    ).text
+                url = "https://buscacepinter.correios.com.br/app/endereco/index.php"
+
+                drive.get(url)
+                pyautogui.sleep(2)
+                #data = cep_input.get()
+                drive.find_element(By.NAME, "endereco").send_keys(data)
+                pyautogui.sleep(seconds=0.5)
+                #data = cep_input.get()
+                
+                pyautogui.sleep(1)
+                drive.find_element(By.NAME, "btn_pesquisar").click()
+                pyautogui.sleep(1)
+
+
+                logradouro = drive.find_element(
+                    By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[1]'
+                ).text
+                
+                pyautogui.sleep(1)
+
+                bairro = drive.find_element(
+                    By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[2]'
+                ).text
+
+                pyautogui.sleep(1)
+
+                localidade = drive.find_element(
+                    By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[3]'
+                ).text
+            
+                pyautogui.sleep(1)
+
+                cep = drive.find_element(
+                    By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[4]'
+                ).text
+            
+                pyautogui.sleep(3)
+
+                tree_view.insert('',END,values= (logradouro,bairro,localidade,cep))
+
+                drive.find_element(By.XPATH,'//*[@id="btn_nbusca"]').click()
+
+                pyautogui.sleep(2)
+
     
-    pyautogui.sleep(1)
+        #root.after(2000, lambda: cep_input.delete(0, END))
 
-    bairro = drive.find_element(
-        By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[2]'
-    ).text
-    bairro__label.configure(text=bairro)
-    pyautogui.sleep(1)
 
-    localidade = drive.find_element(
-        By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[3]'
-    ).text
-    localidade__label.configure(text=localidade)
-    pyautogui.sleep(1)
 
-    cep = drive.find_element(
-        By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[4]'
-    ).text
-    cep__label.configure(text=cep)
-    pyautogui.sleep(1)
-
-  
-    root.after(2000, lambda: cep_input.delete(0, END))
-
-cep_label = CTkLabel(root, text="CEP", text_color="white", font=("Georgia", 20))
+cep_label = CTkLabel(root, text="CEP", text_color="white", font=("Sans", 20))
 cep_label.grid(row=1, column=0,stick="NSEW")
-cep_input = CTkEntry(root, text_color="black", font=("Georgia", 20), fg_color="#FFFAFA")
+cep_input = CTkEntry(root, text_color="black", font=("Sans", 20), fg_color="#FFFAFA")
 cep_input.grid(row=1, column=1, columnspan=5,stick="NSEW")
-cep_search = CTkButton(root, text="Pesquisar", font=("Georgia", 20),command=search)
+cep_input.focus()
+cep_search = CTkButton(root, text="Pesquisar", font=("Sans", 20),command=search)
 cep_search.grid(row=1, column=6, padx=10, stick="NSEW")
+auto_search = CTkButton(root, text="AutoBusca", font=("Sans", 20),command=automatic_search)
+auto_search.grid(row=1, column=8, padx=10, stick="NSEW")
 
 # bg_color = root._apply_appearance_mode(ThemeManager.theme["CTkFrame"]["fg_color"])
-text_color = root._apply_appearance_mode(ThemeManager.theme["CTkLabel"]["text_color"])
+#text_color = root._apply_appearance_mode(ThemeManager.theme["CTkLabel"]["text_color"])
 selected_color = root._apply_appearance_mode(
     ThemeManager.theme["CTkButton"]["fg_color"]
 )
@@ -82,10 +116,10 @@ treestyle = ttk.Style(root)
 treestyle.theme_use("alt")
 treestyle.configure(
     "Treeview",
-    foreground=text_color,
+    foreground='black',
     fieldbackground="#FFFAFA",
     borderwidth=3,
-    font=("Georgia", 15),
+    font=("Sans", 10), sep='.'
 )
 
 tree_view = ttk.Treeview(root, columns=(1, 2, 3, 4), show="headings")
@@ -98,6 +132,6 @@ tree_view.heading(3, text="Localidade/UF")
 tree_view.column(4, anchor="center")
 tree_view.heading(4, text="CEP")
 
-tree_view.grid(row=2, column=0, columnspan=12, rowspan=12, stick="NSEW")
+tree_view.grid(row=2, column=0, columnspan=14, stick="NSEW")
 
 root.mainloop()
